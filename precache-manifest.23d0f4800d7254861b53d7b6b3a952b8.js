70b13e2dbf54f5ea6bcdd4203e56487364656827
FaisalMashuri <faisal.mashuri18143@student.unsika.ac.id>
2020-08-11 14:21:13 +0700

    Updates

---
+++ precache-manifest.23d0f4800d7254861b53d7b6b3a952b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bcbcb0fa4800d4a930f8246cfc230e",
+    "url": "/MovieDb/index.html"
+  },
+  {
+    "revision": "6d5c574ddf4d56abcec6",
+    "url": "/MovieDb/static/css/main.e84609d2.chunk.css"
+  },
+  {
+    "revision": "930387459d4626d49c05",
+    "url": "/MovieDb/static/js/2.d3c699e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieDb/static/js/2.d3c699e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5c574ddf4d56abcec6",
+    "url": "/MovieDb/static/js/main.f2650f78.chunk.js"
+  },
+  {
+    "revision": "f8f05135a6677514edcf",
+    "url": "/MovieDb/static/js/runtime-main.ad912b88.js"
+  }
+]);
